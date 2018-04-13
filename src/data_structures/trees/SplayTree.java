bc3561656c4ab86ac58b9d4b8a2cd0bd06b0969c
sevic69 <sebdeveloper6952@gmail.com>
2018-04-12 23:42:02 -0600

    Method splay() of SplayTree class.

--- src/data_structures/trees/SplayTree.java
+++ src/data_structures/trees/SplayTree.java
@@ -36,8 +36,66 @@ public class SplayTree<K extends Comparable<K>, V> extends BinarySearchTree<K,V>
         return v;
     }
     
+    /**
+     * Performs a sequence of rotations until the provided node is the root of
+     * the tree.
+     * @param node Node to splay.
+     * pre: node is not the root of the tree.
+     * post: node is the root of the tree.
+     */
     protected void splay(BinaryTree<Entry<K,V>> node)
     {
-        BinaryTree<Entry<K,V>> parent = null;
+        if(node.equals(root) || node == null) return;
+        // get the parent and grandparent of this node
+        BinaryTree<Entry<K,V>> gp = null;
+        BinaryTree<Entry<K,V>> p = null;
+        boolean hasGp = false;
+        while(node.parent() != null)
+        {
+            p = node.parent();
+            if(p != null) gp = p.parent();
+            hasGp = gp != null;
+            // <editor-fold defaultstate="collapsed" desc="Node is left child">
+            if (node.isLeftChild()) {
+                if (hasGp) // node has a grandparent
+                {
+                    if (p.isLeftChild()) // left-left
+                    {
+                        gp.rotateRight();
+                        p.rotateRight();
+                    } else // left-right
+                    {
+                        p.rotateRight();
+                        gp.rotateLeft();
+                    }
+                } else // left; node has only parent.
+                {                    
+                    p.rotateRight();
+                }
+            } // </editor-fold>
+            // <editor-fold defaultstate="collapsed" desc="Node is right child">
+            else // node is right child
+            {
+                if (hasGp) // node has a grandparent
+                {
+                    if (p.isLeftChild()) // right-left
+                    {
+                        p.rotateLeft();
+                        gp.rotateRight();
+                    } else // right-right
+                    {
+                        gp.rotateLeft();
+                        p.rotateLeft();
+                    }
+                } else // right; node has only parent.
+                {                    
+                    p.rotateLeft();
+                }
+            }
+
+// </editor-fold>
+        }
+        // the splayed node is now the root of the tree
+        root = node;
     }
 }
